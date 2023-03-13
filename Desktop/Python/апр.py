93a4026695597c19130136cec502530cea13515f
Vanhous2005 <v_dremov@list.ru>
2023-03-13 17:05:03 +0400

    Закончил

---
+++ Desktop/Python/апр.py
@@ -0,0 +1,36 @@
+# Task 1
+
+# n = input()
+
+# a = int(n[0]) + int(n[1]) + int(n[-1])
+
+# print(a)
+
+# Task 2
+
+# s = int(input())
+# kate = s // 2
+# pete = kate // 2
+# sergey = kate // 2
+
+# print(kate, pete, sergey)
+
+# Task 3
+
+# ticket = input()
+
+# if int(ticket[0]) + int(ticket[1]) + int(ticket[2]) == int(ticket[3]) + int(ticket[4]) + int(ticket[5]):
+#     print('Счастливый билет')
+# else:
+#     print("Не счастливый")
+
+# Task 4
+
+# n = int(input())
+# m = int(input())
+# k = int(input())
+
+# if k % n == 0 or k % m == 0:
+#     print('YES')
+# else:
+#     print("NO")
