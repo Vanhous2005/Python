af50dc48c574d8a2a4dfe4886f4074b345ee37fd
Vanhous2005 <v_dremov@list.ru>
2023-03-16 15:11:03 +0400

    Закончил

---
+++ Desktop/Python/second.py
@@ -0,0 +1,36 @@
+# Task 1
+
+# n = int(input())
+# x = int(input())
+# y = n - x
+# cn = 0
+
+# while True:
+#     if x > y:
+#         y -= 1
+#         cn += 1
+#     else:
+#         x -= 1
+#         cn += 1
+#     if x == 0 or y == 0:
+#         break
+# print(cn)
+
+# Task 2
+
+# s = int(input())
+# p = int(input())
+
+# for i in range(1001):
+#     for j in range(1001):
+#         if i + j == s and i * j == p:
+#             print(i, j)
+
+# Task 3
+
+# n = int(input())
+
+# for i in range(1, 10000):
+#     if 2 ** i > n:
+#         break
+#     print(2 ** i)
