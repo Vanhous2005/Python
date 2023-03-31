83fb2efa02806c03b340c897294768d79d12129e
Vanhous2005 <v_dremov@list.ru>
2023-03-31 13:24:57 +0400

    Закончил

---
+++ Desktop/Python/5.py
@@ -0,0 +1,22 @@
+#Task 1
+
+#def summa(a, b):
+#    if a == 0:
+#        return b
+#    return summa(a-1, b+1)
+#
+#
+#if __name__ == '__main__':
+#   print(summa(*list(map(int, input().split()))))
+
+#Task 2
+
+#def power(base, exp):
+#    if (exp == 1):
+#        return (base)
+#    if (exp != 1):
+#        return (base * power(base, exp - 1))
+#
+#
+#if __name__ == '__main__':
+#    print(power(int(input()), int(input())))
